c9dcac2e59792e0c05c9b062942720ff6dab3533
Artem <ayarem21@gmail.com>
2020-07-23 13:07:36 +0300

    prepare format data

--- OpenFileCVS.rb
+++ OpenFileCVS.rb
@@ -1,1 +1,9 @@
-puts("test")
+require 'csv'
+
+array_csv = CSV.read("acme_worksheet.csv")
+table = CSV.parse(File.read("acme_worksheet.csv"), headers: true)
+if Date.parse(table[0]["Date"]).mon / 10 == 0
+  print(Date.parse(table[0]["Date"]).year, '-0',Date.parse(table[0]["Date"]).mon, '-',Date.parse(table[0]["Date"]).mday)
+else
+  print(Date.parse(table[0]["Date"]).year, '-',Date.parse(table[0]["Date"]).mon, '-',Date.parse(table[0]["Date"]).mday)
+end
