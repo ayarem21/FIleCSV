9b652aed109de176ac069c2ca339440d568da68c
Artem <ayarem21@gmail.com>
2020-07-24 18:56:48 +0300

    Finds who worked on a certain day and who did not

--- OpenFileCVS.rb
+++ OpenFileCVS.rb
@@ -39,7 +39,7 @@ write_array = []
 new_name_list = []
 new_date_list = []
 new_work_list = []
-not_formatted_data.each_with_index do |dt|
+not_formatted_data.each do |dt|
   cnt = table["Date"].count(dt)
   index.upto(index + cnt - 1) do |index|
     tmp_name.push(table["Employee Name"][index])
@@ -50,44 +50,58 @@ not_formatted_data.each_with_index do |dt|
   str2 = ""
   bool = ""
   # puts tmp_name
-  # puts tmp_work
+  puts tmp_work.length
+  puts tmp_name.length
+
+  # puts tmp_work.lenght
   puts "#{index} #{index+cnt}"
-  employee_name.each do |e_name|
-    tmp_work.each do |work|
-      tmp_date.each do |tmp|
-      # print work
+  employee_name.each_with_index do |e_name, k|
         if tmp_name.include?(e_name)
           str =  e_name  #{tmp} true"
-          str2 = tmp
-          bool = work.to_f
+          # str2 = tmp
+          bool = 1
         else
           str = e_name #{tmp} false"
-          str2 = tmp
+          # str2 = tmp
           bool = 0
         end
       # write_array = write_array.unshift("#{e_name}")
-      end
-    end
+    # end
+
     new_name_list << str
     new_date_list << str2
     new_work_list << bool
   end
+  # print tmp_work
   index += cnt
   tmp_date = []
   tmp_name = []
+  # print tmp_work
   tmp_work = []
 end
 # print(new_name_list, " ", new_date_list, " ", new_work_list)
-
+print (new_work_list)
 new_hash_data = []
 
 new_name_list.each_with_index do |name, i|
   new_hash_data << {"Employee Name" => name, "Date" => new_date_list[i], "Work Hours" => new_work_list[i]}
 end
 
-
+# work_hash = []
+# hour = 0
+# new_hash_data.each_with_index do |hd, i|
+#   if hd["Work Hours"] > 0
+#     hour = table["Work Hours"][i]
+#   else
+#     hour = 0
+#   end
+#   work_hash << hour
+# end
+# puts
+# puts
+# print(work_hash, " ")
 array = []
-puts(new_hash_data)
+# puts(new_hash_data)
 CSV.open("csv/accounting.csv", "w",
   write_headers: true,
   headers: formatted_data
@@ -95,13 +109,13 @@ CSV.open("csv/accounting.csv", "w",
     employee_name.each do |name|
       array.push(name)
       new_hash_data.each_with_index do |h_name, i|
-        if h_name["Work Hours"] > 0
+        if h_name["Work Hours"].to_f > 0
           if h_name["Employee Name"] == name
-            array.push(h_name["Work Hours"])
+            array.push(h_name["Work Hours"].to_f)
           end
         else
           if h_name["Employee Name"] == name
-            array.push(h_name["Work Hours"])
+            array.push(h_name["Work Hours"].to_f)
           end
         end
       end
